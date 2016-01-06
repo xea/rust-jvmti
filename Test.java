fba8526149924f42d56e3e61a96836095fb50d7b
Alex Pecsi <alex.pecsi@gmail.com>
2016-01-06 13:18:11 +0000

    Initial commit

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+
+    public static void main(final String[] args) {
+        System.out.println("Lofasz");
+    }
+}
---
+++ tests/testing.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, 4);
+    }
+}
