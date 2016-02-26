e8619492eeee9e2a0175a19275f4ca0d917d0881
Alex Pecsi <alex.pecsi@gmail.com>
2016-02-26 21:45:49 +0000

    clean start again

--- agent/src/handler.rs
+++
@@ -1,4 +0,0 @@
-use super::context::Context;
-
-pub type FnNativeMethodEntry = fn(context: Context) -> ();
-pub type FnMethodExit = fn(context: Context) -> ();
--- jvmti/src/environment/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod jni;
-pub mod jvmti;
