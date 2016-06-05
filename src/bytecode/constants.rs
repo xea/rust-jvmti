a84eadf6de6d52abcdb17b7cebebf7d44fd96912
Alex Pecsi <alex.pecsi@gmail.com>
2016-06-05 23:28:35 +0100

    updated constants

--- src/bytecode/constants.rs
+++ src/bytecode/constants.rs
@@ -3,11 +3,20 @@ use super::class_stream::ReadChunks;
 use super::class_stream::ReadMapper;
 
 pub enum ConstantType {
+    Utf8 { length: u16, bytes: Vec<u8> }, // 1,
     Integer { bytes: u32 }, // 3,
+    Float { bytes: u32 }, // 4,
     Long { high_bytes: u32, low_bytes: u32 }, // 5,
     Double { high_bytes: u32, low_bytes: u32 }, // 6,
     Class { name_index: u16 }, // 7
     String { string_index: u16 }, // 8,
+    FieldRef { class_index: u16, name_and_type_index: u16 }, // 9
+    MethodRef { class_index: u16, name_and_type_index: u16 }, // 10,
+    InterfaceMethodRef { class_index: u16, name_and_type_index: u16 }, // 11
+    NameAndType { name_index: u16, descriptor_index: u16 }, // 12,
+    MethodHandle { reference_kind: u8, reference_index: u16 }, // 15,
+    MethodType { descriptor_index: u16 }, // 16,
+    InvokeDynamic { bootstrap_method_attr_index: u16, name_and_type_index: u16 }, // 18,
     Placeholder,
     Unknown
 }
@@ -29,10 +38,26 @@ impl ConstantType {
             Some(tag) => {
                 match tag {
                     3 => stream.read_map_len(4, |bs| ConstantType::Integer { bytes: bs.get_u32() }),
+                    4 => stream.read_map_len(4, |bs| ConstantType::Float { bytes: bs.get_u32() }),
                     5 => stream.read_map_len(8, |bs| ConstantType::Long { high_bytes: bs.get_u32(), low_bytes: bs.get_u32() }),
                     6 => stream.read_map_len(8, |bs| ConstantType::Double { high_bytes: bs.get_u32(), low_bytes: bs.get_u32() }),
                     7 => stream.read_map_len(2, |bs| ConstantType::Class { name_index: bs.get_u16() }),
                     8 => stream.read_map_len(2, |bs| ConstantType::String { string_index: bs.get_u16() }),
+                    9 => stream.read_map_len(4, |bs| ConstantType::FieldRef { class_index: bs.get_u16(), name_and_type_index: bs.get_u16() }),
+                    10 => stream.read_map_len(4, |bs| ConstantType::MethodRef { class_index: bs.get_u16(), name_and_type_index: bs.get_u16() }),
+                    11 => stream.read_map_len(4, |bs| ConstantType::InterfaceMethodRef { class_index: bs.get_u16(), name_and_type_index: bs.get_u16() }),
+                    12 => stream.read_map_len(4, |bs| ConstantType::NameAndType { name_index: bs.get_u16(), descriptor_index: bs.get_u16() }),
+                    15 => stream.read_map_len(3, |bs| ConstantType::MethodHandle { reference_kind: bs.get_u8(), reference_index: bs.get_u16() }),
+                    16 => stream.read_map_len(2, |bs| ConstantType::MethodType { descriptor_index: bs.get_u16() }),
+                    18 => stream.read_map_len(4, |bs| ConstantType::InvokeDynamic { bootstrap_method_attr_index: bs.get_u16(), name_and_type_index: bs.get_u16() }),
+                    1 => stream.read_map_len(2, |bs| {
+                        let bytes_count = bs.get_u16();
+
+                        match bs.read_n(bytes_count as usize) {
+                            Some(bytes) => ConstantType::Utf8 { length: bytes_count, bytes: bytes },
+                            None => ConstantType::Unknown
+                        }
+                    }),
                     _ => Some(ConstantType::Unknown)
                 }
             },
--- tests/bytecode/constants.rs
+++ tests/bytecode/constants.rs
@@ -23,4 +23,38 @@ mod tests {
             _ => assert!(false)
         }
     }
+
+    #[test]
+    fn it_should_parse_strings() {
+        let bytes = vec![ 0x01, 0x00, 0x00 ];
+        let mut cs = ClassStream::new(&bytes);
+        let oc = ConstantType::parse(&mut cs);
+
+        match oc {
+            Some(c) => match c {
+                ConstantType::Utf8 { length: l, bytes: b } => {
+                    assert_eq!(0, l);
+                    assert_eq!(0, b.len());
+                },
+                _ => assert!(false)
+            },
+            _ => assert!(false)
+        }
+
+        let bytes = vec![ 0x01, 0x00, 0x04, 0x40, 0x41, 0x42, 0x43 ];
+        let mut cs = ClassStream::new(&bytes);
+        let oc = ConstantType::parse(&mut cs);
+
+        match oc {
+            Some(c) => match c {
+                ConstantType::Utf8 { length: l, bytes: b } => {
+                    assert_eq!(4, l);
+                    assert_eq!(4, b.len());
+                    assert_eq!(vec![ 0x40, 0x41, 0x42, 0x43 ], b);
+                },
+                _ => assert!(false)
+            },
+            _ => assert!(false)
+        }
+    }
 }
