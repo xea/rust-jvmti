77524ba8bd4eb95dc3cbef885eff58a07c7d34dc
Alex Pecsi <alex.pecsi@gmail.com>
2016-11-07 11:43:27 +0000

    Removed some legacy code, updated gitignore and updated regeression test script

--- regression-test.sh
+++ regression-test.sh
@@ -15,6 +15,7 @@ clean_test_data
 find ./test-data -name "*.class" | while read CLASSFILE; do
     echo "Checking ${CLASSFILE}"
     OUTFILE="${CLASSFILE}.out.class"
+    RESULTS="regression-results"
     ./target/debug/jvmti write $CLASSFILE 
     HASHES=`md5 -q $CLASSFILE $OUTFILE | paste -s -d " " -`
     read -r -a RESULT <<< $HASHES
@@ -23,7 +24,7 @@ find ./test-data -name "*.class" | while read CLASSFILE; do
         echo " ---------------------- Mismatch found: ${RESULT[0]} ${RESULT[1]} ${CLASSFILE} -----------------------"
         javap -v $CLASSFILE > ./tmp-compare-1
         javap -v $OUTFILE > ./tmp-compare-2
-        diff -u ./tmp-compare-1 ./tmp-compare-2
+        diff -u ./tmp-compare-1 ./tmp-compare-2 >> $RESULTS
     fi
 
     rm -f $OUTFILE ./tmp-compare-1 ./tmp-compare-2
